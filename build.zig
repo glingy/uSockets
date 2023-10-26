89ce5adc051f27dc7ec36263d924ad424b9256bb
Gregory Ling <0b0100011101101100@gmail.com>
2023-10-26 00:45:16 -0500

    Added single threaded support

--- build.zig
+++ build.zig
@@ -41,13 +41,12 @@ pub fn build(b: *std.Build) !void {
     //     "-std=c++17",
     // });
 
-    const uv = b.dependency("uv", .{
-        .target = target,
-        .optimize = optimize,
-        .single_threaded = single_threaded,
-    });
-
     if (target.isWindows()) {
+        const uv = b.dependency("uv", .{
+            .target = target,
+            .optimize = optimize,
+            .single_threaded = single_threaded,
+        });
         lib.linkLibrary(uv.artifact("uv"));
         lib.defineCMacro("LIBUS_USE_LIBUV", null);
     }
